51d82ea786e6e2466678c3240e75bfc06a371225
Vladimirgentov <v@gentov.ru>
2026-06-24 06:16:51 +0300

    Remove app bucket from Terraform resources

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,7 +8,6 @@ output "kittygram_url" {
   value       = "http://${yandex_compute_instance.kittygram.network_interface[0].nat_ip_address}:${var.gateway_port}"
 }
 
-output "app_bucket_name" {
-  description = "Object Storage bucket created for the application"
-  value       = yandex_storage_bucket.kittygram.bucket
-}
+# output "app_bucket_name" {
+#   value = yandex_storage_bucket.kittygram.bucket
+# }
